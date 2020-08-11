a6e15a3a89a76ce2e748e7e8b8ade20817c6d4cf
Carl Hain <crhain@gmail.com>
2020-08-11 13:16:51 -0500

    Add links between existing pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const AboutPage = () => {
     return (
         <div>
             <h1>About Me</h1>
             <p>I'm a fullstack developer living in the Chicago area.  I spend my time developing web applications, Unity games, and running.</p>
+            <p>For for information, see my <Link to="/contact">Contact page</Link></p>
         </div>
     );
 };
