cb27c39e7b983c6290d71986365a897554615d1e
Carl Hain <crhain@gmail.com>
2020-08-11 13:05:28 -0500

    Change some text on about and contact pages

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,7 +3,7 @@ import React from 'react';
 const AboutPage = () => {
     return (
         <div>
-            <h1>About Page</h1>
+            <h1>About Me</h1>
             <p>I'm a fullstack developer living in the Chicago area.  I spend my time developing web applications, Unity games, and running.</p>
         </div>
     );
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,7 @@ import React from 'react';
 const ContactPage = () => {
     return (
         <div>
-            <h1>Contact Page</h1>
+            <h1>Contact</h1>
             <p>See my stuff at <a href="https://github.com/crhwebdev" target="_blank">Github</a></p>
         </div>
     );
