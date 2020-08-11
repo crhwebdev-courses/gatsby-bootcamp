405f533ab293e73dc8f98409070b4d0de48f8363
Carl Hain <crhain@gmail.com>
2020-08-11 13:01:47 -0500

    Add a basic about page and contact page

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+    return (
+        <div>
+            <h1>About Page</h1>
+            <p>I'm a fullstack developer living in the Chicago area.  I spend my time developing web applications, Unity games, and running.</p>
+        </div>
+    );
+};
+
+export default AboutPage;
---
+++ src/pages/contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ContactPage = () => {
+    return (
+        <div>
+            <h1>Contact Page</h1>
+            <p>See my stuff at <a href="https://github.com/crhwebdev" target="_blank">Github</a></p>
+        </div>
+    );
+};
+
+export default ContactPage;
