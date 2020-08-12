a95dfc115af9cb1ff5e18be2972cd3a73377f73b
Carl Hain <crhain@gmail.com>
2020-08-12 12:15:24 -0500

    Install source-file-system plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,13 @@ module.exports = {
     author: 'Carl Hain'
   },
   plugins: [
-    'gatsby-plugin-sass'
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',      
+      options: {
+        name: 'src',  
+        path: `${__dirname}/src/`,
+      }       
+    }
   ],
 }
