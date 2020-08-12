bc479cf9a2334d6695bac0e0a96cf6f227e61c17
Carl Hain <crhain@gmail.com>
2020-08-12 11:27:13 -0500

    Header pulls site title from a graphql query to site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,10 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: 'Full-Stack Bootcamp',
+    author: 'Carl Hain'
+  },
   plugins: [
     'gatsby-plugin-sass'
   ],
--- src/components/header.js
+++ src/components/header.js
@@ -1,15 +1,27 @@
 import React from 'react';
-import { Link } from 'gatsby';
-//import './header.module.scss';
+import { Link, graphql, useStaticQuery } from 'gatsby';
+
 import headerStyles from './header.module.scss';
 
 
 const Header = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            site {
+                siteMetadata {
+                title,
+                author
+            }
+            }
+        }
+    `);
+
+
     return (
         <header className={headerStyles.header}>
             <h1>
                 <Link className={headerStyles.title} to="/">
-                    Gatsby Bootcamp
+                    {data.site.siteMetadata.title}
                 </Link>
                 
             </h1>
